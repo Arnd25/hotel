812b31cc6a206a158cae0cf48eff0b36cebfd27a
Mihail4531 <borovpaziloj4@gmail.com>
2025-06-06 11:32:30 +0300

    mobile

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,4 +1,5 @@
 import Menu from "./plugins/responsive-menu.js";
+import { scrollMenu } from "./plugins/scroll.js";
 
 document.addEventListener("DOMContentLoaded", () => {
     const menu = new Menu({
@@ -6,7 +7,7 @@ document.addEventListener("DOMContentLoaded", () => {
         menuSelector: '[data-menu-overlay]',
         htmlElementSelector: 'html',
         animationDuration: 500,
-        breakpoint: 1140,
-
+        breakpoint: 1300,
     })
+    scrollMenu();
 });
